0b17306e706c907012e9cc658585b63dc5629177
kao.ring <kao.ring@yahoo.com>
2020-08-06 16:54:01 -0400

    added files and folders

---
+++ db/seeds.sql
@@ -0,0 +1,15 @@
+USE burgers
+
+INSERT INTO burgers
+    (burger_name)
+VALUES
+    ("Big Mak"),
+    ("Quarter Pounder with Cheese"),
+    ("Cheeseburger"),
+    ("Double Cheeseburger"),
+    ("MkChicken"),
+    ("Teriyaki-Burger"),
+    ("Filet-yo-Fish");
+
+-- SELECT *
+-- FROM burgers;
