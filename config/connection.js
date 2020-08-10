ab3eaea7d1126a50ca8d97d8a8bd0a9ea9bf65b0
kao.ring <kao.ring@yahoo.com>
2020-08-10 16:04:08 -0400

    DONE

--- config/connection.js
+++ config/connection.js
@@ -1,19 +1,13 @@
 // Set up MySQL connection.
+require("dotenv").config();
 var mysql = require("mysql");
-//jgfkxif863dgz3zx:r2l5c26ya0u04nck@rnr56s6e2uk326pj.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/xdav8squ7355gktj
 
-mysql: mysql: var connection;
+var connection;
 
 if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
-  connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "trilogy!",
-    database: "burgers_db",
-  });
+  connection = mysql.createConnection(process.env.DB_URL);
 }
 
 // Make connection.
