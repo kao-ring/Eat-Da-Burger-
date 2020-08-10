c1460631b796dac9d38157c93e1b641a41500117
kao.ring <kao.ring@yahoo.com>
2020-08-10 15:17:43 -0400

    JAWSDB

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,8 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
-var connection;
+//jgfkxif863dgz3zx:r2l5c26ya0u04nck@rnr56s6e2uk326pj.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/xdav8squ7355gktj
+
+mysql: mysql: var connection;
 
 if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
